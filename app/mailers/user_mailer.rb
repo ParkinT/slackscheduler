67eaefc58d1ff2794bf14dcecb05757dca0cc334
Thom Parkin <Thom.Parkin@Websembly.com>
2016-05-14 23:04:29 +0000

    Updates to Validation email operation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: 'SlackMinder@Websembly.com'
+  default from: 'SlackScheduler@Websembly.com'
 
   def welcome_email(user)
     @user = user
-    @url = 'http://slackminder.herokuapp.com/'
-    mail(to: @user.email, subject: 'Welcome to Slack [re]Minder')
+    @url = 'http://slackscheduler.herokuapp.com/'
+    mail(to: @user.email, subject: 'Welcome to Slack Scheduler')
   end
 
   def notification_email(user, ical)
@@ -12,8 +12,8 @@ class UserMailer < ApplicationMailer
     events = ical.instance_variable_get(:@events)
     @summary = events.first.instance_variable_get(:@summary)
     #mail.attachments['slackminder.ics'] = { mime_type: 'application/ics', content: ical.to_ical }
-    mail.attachments['slackminder.ics'] = { mime_type: 'text/calendar', content: ical.to_ical }
-    mail(to: @user.email, subject: "Slack [re]Minder for #{@summary}")
+    mail.attachments['slackschedule.ics'] = { mime_type: 'text/calendar', content: ical.to_ical }
+    mail(to: @user.email, subject: "Slack Scheduler for #{@summary}")
   end
 end
 
